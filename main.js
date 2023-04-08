8a8d35c0e07f51aa856e15dd3f973edc12689b3f
aberrantian <aberrantian@gmail.com>
2023-04-08 23:59:03 +0100

    refactor error checks

--- main.js
+++ main.js
@@ -13,61 +13,25 @@ const ERROR = document.getElementById("error");
 //   "color: red;width: fit-content;padding: 4px;border: red solid 1px;";
 
 const FORM = document.getElementById("sign-up-form");
-FORM.addEventListener("submit", (event) => {
-  event.preventDefault();
-});
 
-INPUTS.firstName.addEventListener("input", (event) => {
-  const self = INPUTS.firstName;
-  /*
-  IF NOT empty AND only letters
-    valid = true
-  ELSE
-    valid = false
-  */
-});
-INPUTS.lastName.addEventListener("input", (event) => {
-  const self = INPUTS.lastName;
-  /*
-  IF NOT empty AND only letters
-    valid = true
-  ELSE
-    valid = false
-  */
-});
-INPUTS.email.addEventListener("input", (event) => {
-  const self = INPUTS.email;
-  /*
-  IF NOT empty AND valid email
-    valid = true
-  ELSE
-    valid = false
-  */
-});
-INPUTS.phone.addEventListener("input", (event) => {
-  const self = INPUTS.phone;
-  /*
-  IF NOT empty AND only numbers AND valid number
-    valid = true
-  ELSE
-    valid = false
-  */
-});
-INPUTS.password.addEventListener("input", (event) => {
-  const self = INPUTS.password;
-  /*
-  IF NOT empty AND meets criteria
-    valid = true
-  ELSE
-    valid = false
-  */
-});
-INPUTS.confirmPassword.addEventListener("input", (event) => {
-  const self = INPUTS.confirmPassword;
-  /*
-  IF matches password
-    valid = true
-  ELSE
-    valid = false
-  */
+FORM.addEventListener("submit", (event) => {
+  let errorMessages = [];
+
+  if (INPUTS.password.value.length < 10) {
+    errorMessages.push("Password is too short.");
+  }
+
+  if (INPUTS.confirmPassword.value != INPUTS.password.value) {
+    errorMessages.push("Passwords don't match.");
+  }
+
+  if (errorMessages.length > 0) {
+    event.preventDefault();
+    ERROR.innerText = errorMessages.join("\n");
+
+    ERROR.style =
+      "color: red;width: fit-content;padding: 4px;border: red solid 1px;";
+  } else {
+    ERROR.style = "";
+  }
 });
